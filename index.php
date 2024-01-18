070fba1aa3e680584e3fad546e5f2982a4ed9d08
Tien Nguyen <tiennguyen3354@gmail.com>
2024-01-18 12:44:24 -0500

    implemented fat free framework into the page

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Tien Nguyen
+ * January 2024
+ * https://tiennguyen3354.greenriverdev.com/328/pets/
+ * This is my CONTROLLER for the Diner application
+ */
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//accessing the autoload page from the Fat Free Framework
+require_once ('vendor/autoload.php');
+
+// INSTANTIATE THE FAT FREE
+$f3 = Base::instance();
+
+//DEFINE A DEFAULT ROUTE
+$f3->route('GET /' , function (){
+    echo "Pet Home";
+});
+
+//RUN FAT FREE
+$f3->run();
